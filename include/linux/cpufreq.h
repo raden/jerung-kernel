a49e5a6b8669de6076568f0be5d5b027689c6db5
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-07-21 05:58:43 +0800

    Add ondemandplus governor
    Conflicts:
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Conflicts:
    	include/linux/cpufreq.h
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -382,6 +382,9 @@ extern struct cpufreq_governor cpufreq_gov_bluactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandplus)
 #endif
 
 
