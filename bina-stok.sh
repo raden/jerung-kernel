5948977e723d46d7379387a62647fba2eb355db0
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-08-18 16:02:02 +0800

    Jerung v2.0
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -8,7 +8,7 @@ export ARCH=arm
 export CROSS_COMPILE=/opt/old-linaro/arm-linux-gnueabi-linaro_4.8.3-2014.02/bin/arm-linux-gnueabi-
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-Jerung-v1.0"
+export LOCALVERSION="-Jerung-v2.0"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
