f97bd686ac7b885953006f4e5db44e9a4b2c7574
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-08-19 10:01:56 +0800

    compiler build file update
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -10,7 +10,7 @@ export CROSS_COMPILE=/opt/old-linaro/arm-linux-gnueabi-linaro_4.7.4-2014.01/bin/
 #export CROSS_COMPILE=/opt/old-linaro/arm-linux-gnueabi-linaro_4.8.3-2014.02/bin/arm-linux-gnueabi-
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-Jerung-v2.0"
+export LOCALVERSION="-Jerung-v2.1"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
